0ea1051f2211eb3b0c550929efa5b41119338123
Min Seop Kim <k_ms1998@naver.com>
2022-06-30 16:08:54 +0900

    주석 추가

--- src/main/java/fastcampus/GetInLine/constant/ErrorCode.java
+++ src/main/java/fastcampus/GetInLine/constant/ErrorCode.java
@@ -20,11 +20,13 @@ public enum ErrorCode {
     SPRING_BAD_REQUEST(10001, HttpStatus.BAD_REQUEST, "Spring-detected bad request"),
     VALIDATION_ERROR(10002, HttpStatus.BAD_REQUEST, "Validation error"),
     NOT_FOUND(10003, HttpStatus.NOT_FOUND, "Requested resource is not found"),
-
     INTERNAL_ERROR(20000, HttpStatus.INTERNAL_SERVER_ERROR, "Internal error"),
     SPRING_INTERNAL_ERROR(20001, HttpStatus.INTERNAL_SERVER_ERROR, "Spring-detected internal error"),
     DATA_ACCESS_ERROR(20002, HttpStatus.INTERNAL_SERVER_ERROR, "Data access error");
 
+    /**
+     * ENUM(code, httpStatus, message)
+     */
     private final Integer code;
     private final HttpStatus httpStatus;
     private final String message;
