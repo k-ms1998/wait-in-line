645c9c16372e46f079d2d1bb6c5fa8f560453197
Min Seop Kim <k_ms1998@naver.com>
2022-07-06 16:35:49 +0900

    ReadOnlyRepository

--- src/main/java/fastcampus/GetInLine/config/RepositoryConfig.java
+++ src/main/java/fastcampus/GetInLine/config/RepositoryConfig.java
@@ -13,34 +13,38 @@ import java.util.Optional;
 @Configuration
 public class RepositoryConfig {
 
-    @Bean
-    public EventRepository eventRepository() {
-        return new EventRepository() {
-            @Override
-            public List<EventDTO> findEvents(Long placeId, String eventName, EventStatus eventStatus,
-                                             LocalDateTime eventStartDatetime, LocalDateTime eventEndDatetime) {
-                return null;
-            }
-
-            @Override
-            public Optional<EventDTO> findEvent(Long eventId) {
-                return Optional.empty();
-            }
-
-            @Override
-            public Boolean createEvent(EventDTO eventDTO) {
-                return null;
-            }
-
-            @Override
-            public Boolean modifyEvent(Long eventId, EventDTO eventDTO) {
-                return null;
-            }
-
-            @Override
-            public Boolean removeEvent(Long eventId) {
-                return null;
-            }
-        };
-    }
+    /**
+     * EventRepository 가 JPARepository 를 상속받기 때문에 자동으로 빈 등록이 됨
+     * 그러므로, Config 에서 따로 빈 등록하는 부분을 제거
+     */
+//    @Bean
+//    public EventRepository eventRepository() {
+//        return new EventRepository() {
+//            @Override
+//            public List<EventDTO> findEvents(Long placeId, String eventName, EventStatus eventStatus,
+//                                             LocalDateTime eventStartDatetime, LocalDateTime eventEndDatetime) {
+//                return null;
+//            }
+//
+//            @Override
+//            public Optional<EventDTO> findEvent(Long eventId) {
+//                return Optional.empty();
+//            }
+//
+//            @Override
+//            public Boolean createEvent(EventDTO eventDTO) {
+//                return null;
+//            }
+//
+//            @Override
+//            public Boolean modifyEvent(Long eventId, EventDTO eventDTO) {
+//                return null;
+//            }
+//
+//            @Override
+//            public Boolean removeEvent(Long eventId) {
+//                return null;
+//            }
+//        };
+//    }
 }
--- src/main/java/fastcampus/GetInLine/repository/EventRepository.java
+++ src/main/java/fastcampus/GetInLine/repository/EventRepository.java
@@ -1,20 +1,20 @@
 package fastcampus.GetInLine.repository;
 
+import fastcampus.GetInLine.domain.Event;
 import fastcampus.GetInLine.domain.constant.EventStatus;
 import fastcampus.GetInLine.dto.EventDTO;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-public interface EventRepository {
+public interface EventRepository extends EventReadOnlyRepository<Event, Long> {
     List<EventDTO> findEvents(Long placeId,
                               String eventName,
                               EventStatus eventStatus,
                               LocalDateTime eventStartDatetime,
                               LocalDateTime eventEndDatetime);
-
     Optional<EventDTO> findEvent(Long eventId);
 
     Boolean createEvent(EventDTO eventDTO);
