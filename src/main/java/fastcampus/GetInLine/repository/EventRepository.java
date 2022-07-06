ffb7a6189525ec6ca818df03a9b5d9dc5e8f6ebc
Min Seop Kim <k_ms1998@naver.com>
2022-07-06 17:41:36 +0900

    Removed EventReadOnlyRepository

--- src/main/java/fastcampus/GetInLine/repository/EventRepository.java
+++ src/main/java/fastcampus/GetInLine/repository/EventRepository.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-public interface EventRepository extends EventReadOnlyRepository<Event, Long> {
+public interface EventRepository extends JpaRepository<Event, Long> {
     List<EventDTO> findEvents(Long placeId,
                               String eventName,
                               EventStatus eventStatus,
