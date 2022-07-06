ec4de4fed5f74119ea42d850a7ce367e9abcfd7f
Min Seop Kim <k_ms1998@naver.com>
2022-07-06 16:36:41 +0900

    Enabling Auditing

--- src/main/java/fastcampus/GetInLine/GetInLineApplication.java
+++ src/main/java/fastcampus/GetInLine/GetInLineApplication.java
@@ -3,9 +3,11 @@ package fastcampus.GetInLine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@ConfigurationPropertiesScan
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableJpaAuditing
 public class GetInLineApplication {
 
 	public static void main(String[] args) {
